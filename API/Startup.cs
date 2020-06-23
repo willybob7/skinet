d74446cfbb630ad521f2737d161029afb2b0beff
willybob7 <34104463+willybob7@users.noreply.github.com>
2020-06-22 23:12:07 -0400

    endOfSection3

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,3 +1,4 @@
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -24,7 +25,8 @@ namespace API
         {
             services.AddControllers();
             services.AddDbContext<StoreContext>
-                (x => x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
+                (x => x.UseLazyLoadingProxies().UseSqlite(_config.GetConnectionString("DefaultConnection")));
+            services.AddScoped<IProductRepository, ProductRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
--- Core/Entities/Product.cs
+++ Core/Entities/Product.cs
@@ -2,10 +2,19 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Core.Entities
 {
-    public class Product
+    public class Product : BaseEntity
     {
-        public int Id { get; set; }
+        // public int Id { get; set; }
         [MaxLength(100)]
         public string Name { get; set; }
+        [MaxLength(300)]
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        [MaxLength(100)]
+        public string PictureUrl { get; set; }
+        public virtual ProductType ProductType { get; set; }
+        public int ProductTypeId { get; set; }
+        public virtual ProductBrand ProductBrand { get; set; }
+        public int ProductBrandId { get; set; }
     }
 }
